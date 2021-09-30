81637bc2f3e9f6da9a796ae3474463f65ff873ec
Justyna <justynka.takk@gmail.com>
2021-09-30 16:48:23 +0200

    feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,11 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: 'Your post',
+        content:
+          'Why did Star Wars episodes 4, 5, 6 come before 1, 2, 3? Because in charge of planning, Yoda was.',
+      },
+    ],
+  });
+};

03797a8b02b572c2ecdc5da09f6b47d5be4864b3
Justyna <justynka.takk@gmail.com>
2021-09-30 18:37:00 +0200

    CORS set

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);

74b4a7b787afcc270d73ca301d5779062798df9c
Justyna <justynka.takk@gmail.com>
2021-10-02 09:41:45 +0200

    server and app added

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
 const express = require('express');
 
-const feedRoutes = require('./routes/feed');
-
 const app = express();
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.use(express.json());
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(200).json({
+    message: 'It works',
+  });
   next();
 });
 
-app.use('/feed', feedRoutes);
-
-app.listen(8080);
+module.exports = app;

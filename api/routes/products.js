95d59683e9380eea04ca61578d854ea3e22b8643
Justyna <justynka.takk@gmail.com>
2021-10-07 18:44:37 +0200

    adding mongoose paths to db

--- api/routes/products.js
+++ api/routes/products.js
@@ -63,15 +63,34 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product',
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product',
-  });
+  const id = req.params.productId;
+  Product.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;

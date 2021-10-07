d91415a95b0d7553235918e5302755e22488168b
Justyna <justynka.takk@gmail.com>
2021-10-07 09:24:32 +0200

    adding morgan

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-//const port = process.env.PORT || 5000;
-const port = 5050;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 

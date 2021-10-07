897e153ce5ee50f286a657d0a5d322ca69fce15b
Justyna <justynka.takk@gmail.com>
2021-10-07 09:34:11 +0200

    error handling

--- app.js
+++ app.js
@@ -10,10 +10,19 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-// app.use((req, res, next) => {
-//   res.status(200).json({
-//     message: 'It works',
-//   });
-// });
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;

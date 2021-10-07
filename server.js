654128a6e035a299e520f6a6dacfb7391c8fd863
Justyna <justynka.takk@gmail.com>
2021-10-07 08:02:28 +0200

    orders and products routes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+//const port = process.env.PORT || 5000;
+const port = 5050;
 
 const server = http.createServer(app);
 

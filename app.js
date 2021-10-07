8be20a9b1cbef6d8f29a697cd4d5005f8ed9dd13
Justyna <justynka.takk@gmail.com>
2021-10-07 10:42:33 +0200

    adding cors

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
